4b417d6c4aba8515720b4dc12df127a724bb419f
H. M. Tariq <muhammadtariq2001@outlook.com>
2024-06-24 02:05:48 +0500

    Project is done.

--- test_app.py
+++ test_app.py
@@ -12,7 +12,7 @@ st.title("Urbanmop HR Project")
 # Dictionary to keep track of renamed files
 dataframe_dict = {}
 
-# Create two rows with three columns each for the file uploader
+# Create two rows with three columns each for the file uploaders
 cols = st.columns(3)
 
 with cols[0]:
@@ -38,8 +38,8 @@ with cols[1]:
     cleaners_history_file = st.file_uploader("Cleaners History File", type=['csv', 'xlsx'], key="cleaners_history")
 
 with cols[2]:
-    st.header("L27 File")
-    l27_hr_file = st.file_uploader("L27 File", type=['csv', 'xlsx'], key="l27_hr")
+    st.header("L27 HR File")
+    l27_hr_file = st.file_uploader("L27 HR File", type=['csv', 'xlsx'], key="l27_hr")
 
 
 # Function to process and verify each uploaded file
@@ -51,30 +51,23 @@ def process_file(file, name):
             return None
         flag = match_columns_to_key(list(df.columns))
         if flag is None:
-            st.error(f"Error in identification of {name} file. Missing required columns.")
+            st.error(f"Error in identification of {name} file.")
             return None
         dataframe_dict[name] = df
-        return True
-    return False
 
 
 # Process each uploaded file
-file_status = {
-    'Calendly': process_file(calendly_file, 'Calendly'),
-    'Phone Interview': process_file(phone_interview_file, 'Phone Interview'),
-    'Contract': process_file(contract_file, 'Contract'),
-    'Endorsement': process_file(endorsement_file, 'Endorsement'),
-    'Cleaners History': process_file(cleaners_history_file, 'Cleaners History'),
-    'L27 HR': process_file(l27_hr_file, 'L27 HR')
-}
-
-# Check the status of all files
-all_files_uploaded = all(file_status.values())
+process_file(calendly_file, 'Calendly')
+process_file(phone_interview_file, 'Phone Interview')
+process_file(contract_file, 'Contract')
+process_file(endorsement_file, 'Endorsement')
+process_file(cleaners_history_file, 'Cleaners History')
+process_file(l27_hr_file, 'L27 HR')
 
 # Ensure all files are uploaded before processing
 if st.button("Process"):
-    if not all_files_uploaded:
-        st.error("Please upload all six files and ensure they have the correct columns.")
+    if len(dataframe_dict) != 6:
+        st.error("Please upload all six files.")
     else:
         main(
             dataframe_dict['Calendly'],
